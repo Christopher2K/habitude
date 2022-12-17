5a7eeeaf55a577ff60ed71035bbca50b699d3267
Christopher N. KATOYI KABA <me@christopher2k.dev>
2022-12-17 06:13:10 +0100

    chore: initial

---
+++ src/screens/Home.tsx
@@ -0,0 +1,9 @@
+import { View, Text } from "react-native";
+
+export const Home = () => {
+  return (
+    <View style={{ justifyContent: "center", alignItems: "center", flex: 1 }}>
+      <Text>Hello</Text>
+    </View>
+  );
+};
